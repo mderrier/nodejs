a11d9392c45a1e75cabb27f5041e7a0d4a73fc65
mderrier <mderrier@users.noreply.github.com>
2017-01-10 22:51:22 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -9,11 +9,11 @@ issues_url 'https://github.com/redguide/nodejs/issues' if respond_to?(:issues_ur
 chef_version '>= 11.0' if respond_to?(:chef_version)
 version '2.4.4'
 
-depends 'yum-epel'
-depends 'build-essential'
-depends 'ark'
-depends 'apt'
-depends 'homebrew'
+depends 'yum-epel', '= 0.7.1'
+depends 'build-essential', '= 3.2.0'
+depends 'ark', '= 1.2.0'
+depends 'apt', '= 3.0.0'
+depends 'homebrew', '= 2.1.2'
 
 %w(debian ubuntu centos redhat scientific oracle amazon smartos mac_os_x).each do |os|
   supports os
